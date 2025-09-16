949b28dda645d84b2c13aef38081d0cc7835619c
miyateru <arthur.mendes@estudante.ufla.br>
2025-09-16 00:14:42 -0300

    Semana 05 - Problema 07
    Resolução Questão 7: Arquivo - Dobro. Nota 100.

---
+++ Semana05/Problema07/problema07.cpp
@@ -0,0 +1,35 @@
+//IAlg - Semana 05 - Problema 07
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int num = 0, prevNum = 0, quan = 0;
+	string nomeArquivo = " ";
+	
+	cin >> nomeArquivo;
+	
+	ifstream entrada(nomeArquivo);
+	
+	if (entrada) {
+		entrada >> prevNum;
+		while (entrada >> num) {
+			if (num == prevNum*2) {
+				quan++;
+			}
+			prevNum = num;
+		}
+		
+		ofstream saida("dobro.txt");
+		saida << quan << endl;
+		saida.close();
+	}
+	else {
+		cout << "Arquivo não encontrado: " << nomeArquivo << endl;
+	}
+	
+	entrada.close();
+	return 0;
+}
