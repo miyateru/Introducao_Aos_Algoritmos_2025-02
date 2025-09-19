1d5bf8b208fc4f0feda1b3eb8435612d8bed1c49
miyateru <arthur.mendes@estudante.ufla.br>
2025-09-19 13:02:49 -0300

    Update problema09.cpp
    Signed-off-by: miyateru <arthur.mendes@estudante.ufla.br>

--- Semana05/Problema09/problema09.cpp
+++ Semana05/Problema09/problema09.cpp
@@ -5,13 +5,59 @@
 
 using namespace std;
 
+int convertePont(string tentativa) {
+    const string strike = "VERDE", naoStrike = "VERMELHO";
+    if (strike.compare(tentativa) == 0) {
+        return 1;
+    }
+    else if (naoStrike.compare(tentativa) == 0) {
+        return 0;
+    }
+    return 0;
+}
+
+bool rodadaGanha(int pontJog1, int pontJog2) {
+    if (pontJog1 > pontJog2) {
+        cout << "Jogador 1 é o ganhador da aposta" << endl;
+        return true;
+    }
+    else if (pontJog2 > pontJog1) {
+        cout << "Jogador 2 é o ganhador da aposta" << endl;
+        return true;
+    }
+    return false;
+}
+
+void pontRodada(string tentativa, int& pont) {
+    pont += convertePont(tentativa);
+    return;
+}
+
 int main() {
+    bool ganhador = false;
     int pontJog1 = 0, pontJog2 = 0;
-    string strike = "VERDE", naoStrike = "VERMELHO";
+    string tentJog1 = " ", tentJog2 = " ";
 
     ifstream entrada("boliche.txt");
     if (entrada) {
+        int i = 10;
+        while ((entrada >> tentJog1 >> tentJog2) and (i > 0)) {
+            pontRodada(tentJog1, pontJog1);
+            pontRodada(tentJog2, pontJog2);
+            i--;
+        }
         
+        if (rodadaGanha(pontJog1, pontJog2)) {
+            return 0;
+        }
+         
+        while ((entrada >> tentJog1 >> tentJog2) and (ganhador == false)) {
+            pontRodada(tentJog1, pontJog1);
+            pontRodada(tentJog2, pontJog2);
+            ganhador = rodadaGanha(pontJog1, pontJog2);
+        }
+        
+        return 0;
     }
     else {
         cout << "Arquivo não encontrado: \"boliche.txt\"" << endl;
